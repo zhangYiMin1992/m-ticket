3c97cd8a375e71dc6bc6e29ff62a589ee592d07e
simple <470779536@qq.com>
2018-08-20 18:25:01 +0800

    安装vue-awesome

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -2,15 +2,19 @@
     <div>
         <header class="header-box" id="header">
             <span class="header-back">
-               <i class="icon-title-back effect-active"></i> 
+               <icon name="chevron-left"></icon>
             </span>
             <h1><span>景点门票</span></h1>
         </header>
     </div>
 </template>
 <script>
+import "vue-awesome/icons"
+import Icon from "vue-awesome/components/Icon"
 export default {
-    
+    components:{
+        Icon
+    }
 }
 </script>
 <style lang="scss" scoped>
