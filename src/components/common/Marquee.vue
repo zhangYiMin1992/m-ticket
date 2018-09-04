0f1f3c2c737fa41bc223d24b36340508f2a740d5
simple <470779536@qq.com>
2018-09-04 18:31:16 +0800

    swipe

---
+++ src/components/common/Marquee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="zzui-broascast-window">
+        <ul>
+            <slot></slot>
+        </ul>
+    </div>    
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
